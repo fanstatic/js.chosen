024a88fac0b655d8ffb562efd35ebd65d39c4265
Steffen Allner <sa@gocept.com>
2017-09-28 10:14:37 +0200

    Update to version 1.8.1 of chosen.

--- js/chosen/__init__.py
+++ js/chosen/__init__.py
@@ -3,7 +3,9 @@ import js.jquery
 
 library = fanstatic.Library('chosen', 'resources')
 
-chosen_css = fanstatic.Resource(library, 'chosen.css')
+chosen_css = fanstatic.Resource(
+    library, 'chosen.css', minified='chosen.min.css')
 
-chosen_jquery = fanstatic.Resource(library, 'chosen.jquery.js',
+chosen_jquery = fanstatic.Resource(
+    library, 'chosen.jquery.js',
     minified='chosen.jquery.min.js', depends=[js.jquery.jquery, chosen_css])
